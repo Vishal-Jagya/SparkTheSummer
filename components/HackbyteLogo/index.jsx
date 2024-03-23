b3f99c61c404bc78221f6e3f4d1d24ca771f87f3
fenil <fenilmodi4820@gmail.com>
2024-03-23 17:03:34 +0530

    added firabase

--- components/HackbyteLogo/index.jsx
+++ components/HackbyteLogo/index.jsx
@@ -3,7 +3,7 @@
 import { useEffect, useRef } from "react";
 import Sketch from "./sktech";
 import Image from "next/image";
-import hackbyteLogo from "@/public/s2.jpg";
+import hackbyteLogo from "@/public/sts3.png";
 import spsLogo from "@/public/SPS.jpg";
 
 const HackbyteLogo = () => {
@@ -48,7 +48,7 @@ const HackbyteLogo = () => {
         data-strength="0.18"
       >
         <img
-          src={"/s2.jpg"}
+          src={"/sts3.png"}
           alt="HackByte Logo"
           id="hackbyteLogo"
           className="invisible"
