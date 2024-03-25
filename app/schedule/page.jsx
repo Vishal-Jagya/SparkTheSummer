8ad54863108ce2862a61e0f5c022a30a7a4a1ed8
codingarpan <contact.codingarpan@gmail.com>
2024-03-25 19:21:18 +0530

    new background added

--- app/schedule/page.jsx
+++ app/schedule/page.jsx
@@ -13,7 +13,7 @@ export default function Schedule() {
         className="flex flex-col min-h-screen bg-cover p-4 md:px-12 md:pt-24 pt-16"
         style={{
           backgroundColor: "#000",
-          backgroundImage: "url(/background.png)",
+          backgroundImage: "url(/newbackground.png)",
           backgroundAttachment: "fixed",
         }}
       >
